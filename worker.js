a0ca3a28f7ae14c5f2a387c93c88d649be7b29c9
FYLSen <FYLSen@users.noreply.github.com>
2023-08-26 06:00:45 +0800

    Refactor data structure for improved efficiency.

--- worker.js
+++ worker.js
@@ -1,105 +1,111 @@
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
+  event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
-    const url = new URL(request.url)
-    const targetUrls = decodeURIComponent(url.searchParams.get('url')).split('|')
-    const regexPattern = decodeURIComponent(url.searchParams.get('RegExp'))
-    const regex = new RegExp(regexPattern)
+  const url = new URL(request.url)
+  const targetUrls = decodeURIComponent(url.searchParams.get('url')).split('|')
+  const regexPattern = decodeURIComponent(url.searchParams.get('RegExp'))
+  const regex = new RegExp(regexPattern)
 
-    const fetchOptions = {
-        headers: request.headers
+  const fetchOptions = {
+    headers: request.headers
+  }
+
+  const lineFilter = new LineFilter(regex)
+  const errors = []
+
+  for(const targetUrl of targetUrls) {
+    try {
+      const response = await fetch(targetUrl, fetchOptions)
+      const reader = response.body.getReader()
+      const decoder = new TextDecoder()
+      await lineFilter.filterStream(reader, decoder)
+    } catch (error) {
+      errors.push(`Error fetching ${targetUrl}: ${error.message}`)
     }
+  }
 
-    const filteredLines = []
-    const errors = []
+  if(errors.length > 0) {
+    return new Response(errors.join('\n'), { status: 500 })
+  }
 
-    for (const targetUrl of targetUrls) {
-        try {
-            const response = await fetch(targetUrl, fetchOptions)
-            const reader = response.body.getReader()
-            const decoder = new TextDecoder()
-            const filtered = await filterStream(reader, decoder, regex)
-            filteredLines.push(...filtered)
-        } catch (error) {
-            errors.push(`Error fetching ${targetUrl}: ${error.message}`)
-        }
-    }
-
-    if (errors.length > 0) {
-        return new Response(errors.join('\n'), { status: 500 })
-    }
-
-    return new Response(filteredLines.join('\n'), { status: 200 })
+  return new Response(lineFilter.getFilteredLines().join('\n'), { status: 200 })
 }
 
-async function filterStream(reader, decoder, regex) {
-    const filteredLines = []
-    let partialLine = ''
+class LineFilter {
+  constructor(regex) {
+    this.regex = regex
+    this.filteredLines = []
+    this.partialLine = ''
+  }
 
-    while (true) {
-        const { done, value } = await reader.read()
+  getFilteredLines() {
+    return this.filteredLines
+  }
 
-        if (done) {
-            break
-        }
+  async filterStream(reader, decoder) {
+    while(true) {
+      const {
+        done,
+        value
+      } = await reader.read()
 
-        const chunk = decoder.decode(value, { stream: true })
-        const lines = chunk.split('\n')
+      if(done) {
+        break
+      }
 
-        if (lines.length > 1) {
-            lines[0] = partialLine + lines[0]
-            partialLine = lines.pop()
-        } else {
-            partialLine += lines[0]
-        }
-        
-        handleLines(lines, filteredLines, regex)
+      const chunk = decoder.decode(value, { stream: true })
+      const lines = chunk.split('\n')
+
+      if(lines.length > 1) {
+        lines[0] = this.partialLine + lines[0]
+        this.partialLine = lines.pop()
+      } else {
+        this.partialLine += lines[0]
+      }
+
+      this.handleLines(lines)
     }
 
-    if (partialLine !== '') {
-        handleLines([partialLine], filteredLines, regex)
+    if(this.partialLine !== '') {
+      this.handleLines([this.partialLine])
     }
+  }
 
-    return filteredLines
-}
+  filterLines(lines) {
+    return lines.filter(line => {
+      const trimmed = line.trim()
+      return trimmed !== '' && !trimmed.startsWith('#') && this.regex.test(line)
+    })
+  }
 
-function filterLines(lines, regex) {
-    return lines.reduce((accum, line) => {
-        const trimmed = line.trim()
-        if (trimmed !== '' && !trimmed.startsWith('#') && regex.test(line)) {
-            accum.push(line)
-        }
-        return accum
-    }, [])
-}
-
-function findProxyGroupIndices(lines) {
+  findProxyGroupIndices(lines) {
     let proxyIndex = -1
     let groupIndex = -1
 
-    for (let i = 0; i < lines.length; i++) {
-        const line = lines[i].trim()
-        if (line === '[Proxy]') {
-            proxyIndex = i
-        } else if (line === '[Proxy Group]') {
-            groupIndex = i
-            break
-        }
+    for(let i = 0; i < lines.length; i++) {
+      const line = lines[i].trim()
+      if(line === '[Proxy]') {
+        proxyIndex = i
+      } else if(line === '[Proxy Group]') {
+        groupIndex = i
+        break
+      }
     }
 
     return [proxyIndex, groupIndex]
-}
+  }
 
-function handleLines(lines, filteredLines, regex) {
-    const [proxyIndex, groupIndex] = findProxyGroupIndices(lines)
-    
-    if (proxyIndex !== -1 && groupIndex !== -1) {
-        filteredLines.push(...filterLines(lines.slice(proxyIndex + 1, groupIndex), regex))
-    } else if (proxyIndex === -1 && groupIndex !== -1) {
-        filteredLines.push(...filterLines(lines.slice(0, groupIndex), regex))
+  handleLines(lines) {
+    const [proxyIndex, groupIndex] = this.findProxyGroupIndices(lines)
+
+    if(proxyIndex !== -1 && groupIndex !== -1) {
+      this.filteredLines.push(...this.filterLines(lines.slice(proxyIndex + 1, groupIndex)))
+    } else if(proxyIndex === -1 && groupIndex !== -1) {
+      this.filteredLines.push(...this.filterLines(lines.slice(0, groupIndex)))
     } else {
-        filteredLines.push(...filterLines(lines, regex))
+      this.filteredLines.push(...this.filterLines(lines))
     }
+  }
 }
